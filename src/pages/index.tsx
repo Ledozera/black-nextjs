fc902216cc965ea263d55d090eaa487f11b57296
Ledozera <eduardoledo11@gmail.com>
2024-08-13 13:44:01 -0300

    Arrumando API URL do card dos produtos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
+import Header from "@/components/Header";
 import type { NextPage } from "next";
+import Link from "next/link";
 import Head from "next/head";
+import { Container, Button } from "reactstrap";
 
 const Home: NextPage = () => {
   return (
@@ -10,14 +13,22 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1>
-          Welcome to <a href="https://nextjs.org" target="blank">Next.js!</a>
-        </h1>
-
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio voluptatem expedita eaque numquam soluta ab voluptatibus, assumenda temporibus autem, inventore, ipsam reprehenderit aperiam sapiente qui enim rerum molestias minima odio.
-        </p>
+      <Header />
+      
+      <main >
+        <Container className="py-5 text-center">
+          <h1 className="mt-5 display-1">
+            O melhor jeito de comprar o que você ama
+          </h1>
+          <p className="my-4">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae iusto voluptatem obcaecati omnis error architecto neque cum exercitationem fugiat. Vero illo autem eum nisi sapiente, odio optio accusamus cupiditate ad.
+          </p>
+          <Link href="/products">
+            <Button color="dark" className="px-4 pb-2">
+              Conheça nossos produtos!
+            </Button>
+          </Link>
+        </Container>
       </main>
     </>
   )
--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,20 +1,41 @@
-import { NextPage } from "next";
-import Head from "next/head";
+import { GetStaticProps, NextPage } from 'next'
+import Head from 'next/head'
+import { ReactNode } from 'react'
+import { Container } from 'reactstrap'
+import Header from '@/components/Header'
+import ProductsList from '@/components/ProductsList'
+import { fetchProducts, ProductType } from '@/services/products'
 
-const Products: NextPage = () => {
-    return (
-        <>
-            <Head>
-                <title>Nossos Produtos</title>
-                <meta name="description" content="Conheça todos os nosso produtos"/>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-
-            <h1>
-                Nossos Produtos
-            </h1>
-        </>
-    )
+export const getStaticProps: GetStaticProps = async () => {
+  const products = await fetchProducts()
+  return { props: { products } }
 }
 
-export default Products;
+const Products: NextPage = (props: {
+  children?: ReactNode
+  products?: ProductType[]
+}) => {
+  return (
+    <>
+      <Head>
+        <title>Nossos Produtos</title>
+        <meta name="description" content="Conheça todos os nossos produtos" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+
+      <main>
+        <Container className="mb-5">
+          <h1 className="my-5">
+            Nossos Produtos
+          </h1>
+
+          {<ProductsList products={props.products!} />}
+        </Container>
+      </main>
+    </>
+  )
+}
+
+export default Products
